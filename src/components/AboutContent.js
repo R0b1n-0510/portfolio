d5fe6717f290b8bcc8deed56fde5669b4394434f
R0b1n-0510 <r0b1nofficial@gmail.com>
2023-01-31 22:03:20 +0530

    initial commit

---
+++ src/components/AboutContent.js
@@ -0,0 +1,32 @@
+import "./AboutContent.css"
+
+import React from 'react'
+import { Link } from "react-router-dom"
+import Img1 from "../assets/five.jpg"
+import Img2 from "../assets/six.jpg"
+
+const AboutContent = () => {
+  return (
+    <div className="about">
+        <div className="left">
+            <h1>Who Am I</h1>
+            <p>I am a dynamic professional with technical proficiency in web development technologies - HTML & CSS, Javascript and React.js</p>
+            <Link to="/contact">
+                <button className="btn">Contact</button>
+            </Link>
+        </div>
+        <div className="right">
+            <div className="img-container">
+                <div className="img-stack top">
+                    <img src={Img1} className="img" alt="true" />
+                </div>
+                <div className="img-stack bottom">
+                    <img src={Img2} className="img" alt="true" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutContent
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/two.jpg"
+import pro2 from "../assets/eight.jpg"
+import pro3 from "../assets/nine.jpg"
+
+const ProjectCardData = [{
+    imgsrc: pro1,
+    title: "FIToMANIA Fitness Classes",
+    text: "A website that provides a platform for people who are losing their control over their health to transform their life and their wellbeing.",
+    view: "https://r0b1n-0510.github.io/my-first-website"
+},
+{
+
+    imgsrc: pro2,
+    title: "Tvastra Healthcare Facilities",
+    text: "The goal was to create a website for online registration for online booking and consultation to doctors to aid treatment and to promote health awareness among the people of our country.",
+    view: "https://tvastra-healthcare.herokuapp.com/"
+
+},
+{
+    imgsrc: pro3,
+    title: "ITSMOVIETIME",
+    text: "A netflix clone project named - ItsMovieTime. It is an AI-Powered Movie Web Application. It combines the desire to unleash powerful creativity with the industry's most advanced JavaScript tools.",
+    view: "https://itsmovietiime.netlify.app/"
+}]
+
+export default ProjectCardData; 
